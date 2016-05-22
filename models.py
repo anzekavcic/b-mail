32e55f4353b700c3bc1428d6a06d25b5018764bf
Anže Kavčič <anzekavcic@Anzes-MacBook-Pro.local>
2016-05-22 22:58:14 +0200

    first commit

---
+++ models.py
@@ -0,0 +1,9 @@
+from google.appengine.ext import ndb
+
+class Bmail(ndb.Model):
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    to = ndb.StringProperty()
+    subject = ndb.StringProperty()
+    msg = ndb.TextProperty()
+    from_user = ndb.StringProperty()
+    read = ndb.BooleanProperty(default=False)
